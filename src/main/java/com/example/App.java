10508a3261bced59b5534b67152bc5ac093b5fc3
rochdi1 <rochdi.abdou3@gmail.com>
2026-08-28 17:13:13 +0200

    test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,8 @@
 package com.example;
-
 public class App {
     public String getGreeting() {
-        return "Hello World from local DevOps Pipeline!";
+        return "Hello World from Java 25!";
     }
-
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,10 @@
 package com.example;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class AppTest {
     @Test
     public void testGreeting() {
-        App app = new App();
-        assertEquals("Hello World from local DevOps Pipeline!", app.getGreeting());
+        App classUnderTest = new App();
+        assertEquals("Hello World from Java 25!", classUnderTest.getGreeting(), "Sollte übereinstimmen");
     }
 }
